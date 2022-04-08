5e5529a808a94ef856e170d657555289a6bd2890
shan242 <94292259+shan242@users.noreply.github.com>
2022-04-08 21:23:09 +0800

    Add files via upload

---
+++ searchBtn.js
@@ -0,0 +1,17 @@
+
+
+$(function () {
+    $("#searchBtn").click(function () {
+        var sn=$.trim($("#search-name").val());
+        // alert(sn);
+        if(sn=="") {
+            alert("名字不能为空");
+            return;
+        }
+        else if(sn=="李朵朵")
+            window.location.href="/liduoduo.html";
+        else
+            alert("抱歉，没有找到你的名字，请踢hxy让他快点写");
+        // window.location("songci.html");
+    });
+});
