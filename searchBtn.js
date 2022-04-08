beab8cd1d1d31ccd2952660c3b6ff8cd842437ad
shan242 <94292259+shan242@users.noreply.github.com>
2022-04-08 21:51:58 +0800

    Update searchBtn.js

--- searchBtn.js
+++ searchBtn.js
@@ -9,7 +9,7 @@ $(function () {
             return;
         }
         else if(sn=="李朵朵")
-            window.location.href="/liduoduo.html";
+            window.location.href="/hxy/liduoduo.html";
         else
             alert("抱歉，没有找到你的名字，请踢hxy让他快点写");
         // window.location("songci.html");
